6accb88e2b2b6a65fc749b24c52b26aec75d4aeb
Alexey Lukashov <Alexey.LUKASHOV@ext.ec.europa.eu>
2015-10-08 08:44:33 +0000

    ESPD-16 commented more code failing in jenkins
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/ESPD/trunk@19 f47dfcff-13ca-4e7d-9efb-eacd05900603

--- src/main/java/eu/grow/espd/controller/DownloadController.java
+++ src/main/java/eu/grow/espd/controller/DownloadController.java
@@ -1,20 +1,7 @@
 package eu.grow.espd.controller;
 
-import java.io.InputStream;
-
-import javax.servlet.ServletOutputStream;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.Marshaller;
-
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
-
-import eu.grow.espd.domain.EspdDocument;
 
 @Controller
 @SessionAttributes("espd")
