4d834488ecc25b78b13967f455eb6117a01f1ee4
Alexey Lukashov <Alexey.LUKASHOV@ext.ec.europa.eu>
2015-10-08 12:45:55 +0000

    ecertisCoreExchangeModel.jar added lo local web-inf
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/ESPD/trunk@24 f47dfcff-13ca-4e7d-9efb-eacd05900603

--- lib/add.cmd
+++
@@ -1,5 +0,0 @@
-set maven=C:\Users\lukasal.NET1\AppData\LocalLow\Home\devtools\apache-maven-3.0.5\bin\mvn.bat
-set lib_path=C:\Users\lukasal.NET1\AppData\LocalLow\Home\workspace.ecertis\ESPD\lib\ecertisExchangeModel.jar
-
-%maven% install:install-file -Dfile=%lib_path% -DgroupId=dgmarkt.ecertis -DartifactId=ecertisExchangeCoreModel -Dversion=0.0.1-SNAPSHOT -Dpackaging=jar -DgeneratePom=true
-
