4b335fe403850d99c8b2e1d483b0cd3e199de1d6
Virgiliu Ratoi <Virgiliu.RATOI@ext.ec.europa.eu>
2015-10-22 15:27:40 +0000

    - registered the locale change interceptor in the web configuration
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/ESPD/trunk@49 f47dfcff-13ca-4e7d-9efb-eacd05900603

--- src/main/java/eu/europa/ec/grow/espd/config/WebConfiguration.java
+++ src/main/java/eu/europa/ec/grow/espd/config/WebConfiguration.java
@@ -2,13 +2,19 @@ package eu.europa.ec.grow.espd.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.view.UrlBasedViewResolver;
 import org.springframework.web.servlet.view.tiles3.TilesConfigurer;
 import org.springframework.web.servlet.view.tiles3.TilesView;
 
+import java.util.Locale;
+
 @Configuration
-class WebConfiguration {
+class WebConfiguration extends WebMvcConfigurerAdapter {
 
 	@Bean
     UrlBasedViewResolver viewResolver() {
@@ -32,6 +38,18 @@ class WebConfiguration {
         LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
         lci.setParamName("lang");
         return lci;
+    }
+
+	@Bean
+	LocaleResolver localeResolver() {
+		CookieLocaleResolver resolver = new CookieLocaleResolver();
+        resolver.setDefaultLocale(Locale.ENGLISH);
+		return resolver;
+	}
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
     }
 
 }
