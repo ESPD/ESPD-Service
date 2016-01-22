c73c1f0a4bd5e0eb7ff0275f5e62a6dfac1cbae7
Virgiliu Ratoi <Virgiliu.RATOI@ext.ec.europa.eu>
2016-01-22 08:24:38 +0000

    - ESPD-37 basic setup for calling TED
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/ESPD/trunk@321 f47dfcff-13ca-4e7d-9efb-eacd05900603

--- espd-web/src/main/java/eu/europa/ec/grow/espd/domain/TedResponse.java
+++ espd-web/src/main/java/eu/europa/ec/grow/espd/domain/TedResponse.java
@@ -1,10 +1,34 @@
 package eu.europa.ec.grow.espd.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+
+import java.util.Map;
 
 /**
  * Created by ratoico on 1/21/16 at 5:08 PM.
  */
 @JsonIgnoreProperties(ignoreUnknown = true)
+@Data
 public class TedResponse {
+
+    private Map<String, TedNotice> info;
+
+    private String noDocOjs;
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    @Data
+    public static class TedNotice {
+
+        private String officialName;
+
+        private String title;
+
+        private String shortDescription;
+
+        private String referenceNumber;
+
+        private String tedUrl;
+
+    }
 }
