e1884c235f20cbddcd57fbece2c0c1ae2da5205c
Alexey Lukashov <Alexey.Lukashov@ext.ec.europa.eu>
2016-06-24 10:44:23 +0200

    ESPD-50 changed link to acceptance

--- espd-web/src/main/java/eu/europa/ec/grow/espd/util/EspdConfiguration.java
+++ espd-web/src/main/java/eu/europa/ec/grow/espd/util/EspdConfiguration.java
@@ -56,10 +56,10 @@ public class EspdConfiguration {
     @Value("${piwik.server:https://webgate.ec.europa.eu/pwar/piwik.php}")
     private String piwikServer;
 
-    @Value("${ecertis.criterion.url:http://wltent03.cc.cec.eu.int:1061/ecertisrest/criteria/espd/[uuid]/?countryFilter=[country]&lang=[lang]}")
+    @Value("${ecertis.criterion.url:https://webgate.acceptance.ec.europa.eu/growth/tools-databases/ecertisrest/criteria/espd/[uuid]/?countryFilter=[country]&lang=[lang]}")
     private String ecertisCriterionURL;
     
-    @Value("${ecertis.evidence.url:https://www.development.ec.europa.eu/growth/tools-databases/ecertis/web/evidence/view/[evidenceID]}")
+    @Value("${ecertis.evidence.url:https://webgate.acceptance.ec.europa.eu/growth/tools-databases/ecertis/web/evidence/view/[evidenceID]}")
     private String ecertisEvidenceURL;
 
     @Autowired
