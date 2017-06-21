aa66415f98ac1225f5d86c26dd470f6300423097
LUKASHOV Alexey <ALEXEY.lukashov@ext.ec.europa.eu>
2017-06-21 13:17:56 +0300

    #75 added requirement subgroups support for criminal convictions

--- espd-web/src/main/java/eu/europa/ec/grow/espd/domain/CriminalConvictionsCriterion.java
+++ espd-web/src/main/java/eu/europa/ec/grow/espd/domain/CriminalConvictionsCriterion.java
@@ -27,20 +27,141 @@ package eu.europa.ec.grow.espd.domain;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+
+import org.apache.commons.collections.CollectionUtils;
+
+import eu.europa.ec.grow.espd.domain.intf.UnboundedRequirementGroup;
 
 @Data
 @EqualsAndHashCode(callSuper = false)
-public class CriminalConvictionsCriterion extends ExclusionCriterion {
+public class CriminalConvictionsCriterion extends ExclusionCriterion implements UnboundedRequirementGroup {
 
-    private Date dateOfConviction;
-    private String reason;
-    private String convicted;
-    private String periodLength;
+    //private Date dateOfConviction;
+    //private String reason;
+    //private String convicted;
+    //private String periodLength;
+	private List<DynamicRequirementGroup> unboundedGroups = new ArrayList<>(1);
 
     public static CriminalConvictionsCriterion buildWithExists(Boolean exists) {
         CriminalConvictionsCriterion criminalConvictions = new CriminalConvictionsCriterion();
         criminalConvictions.setExists(exists);
         return criminalConvictions;
     }
+
+	@Override
+	public List<DynamicRequirementGroup> getUnboundedGroups() {
+		return unboundedGroups;
+	}
+	
+	// Bounded-style settings for were added
+	// to support compatibility with previously written tests
+	// running bounded groups for criminal convictions
+
+	private Object getFirstUnboundedGroupField(String field) {
+		if(CollectionUtils.isEmpty(unboundedGroups)) {
+			return null;
+		}
+		return unboundedGroups.get(0).get(field);
+	}
+
+	private void setFirstUnboundedGroupField(String field, Object value) {
+		if(unboundedGroups == null) {
+			unboundedGroups = new ArrayList<>(1);
+		}
+		if(CollectionUtils.isEmpty(unboundedGroups)) {
+			unboundedGroups.add(new DynamicRequirementGroup());
+		}
+		unboundedGroups.get(0).put(field, value);
+	}
+	
+	public Date getDateOfConviction() {
+		return (Date) getFirstUnboundedGroupField("dateOfConviction");
+	}
+	
+	public String getReason() {
+		return (String) getFirstUnboundedGroupField("reason");
+	}
+	
+	public String getConvicted() {
+		return (String) getFirstUnboundedGroupField("convicted");
+	}
+	
+	public String getPeriodLength() {
+		return (String) getFirstUnboundedGroupField("periodLength");
+	}
+	
+	public void setDateOfConviction(Date value) {
+		setFirstUnboundedGroupField("dateOfConviction", value);
+	}
+	
+	public void setReason(String value) {
+		setFirstUnboundedGroupField("reason", value);
+	}
+	
+	public void setConvicted(String value) {
+		setFirstUnboundedGroupField("convicted", value);
+	}
+	
+	public void setPeriodLength(String value) {
+		setFirstUnboundedGroupField("periodLength", value);
+	}
+	
+	//// Unbounded SelfCleaning support for bounded Tests
+
+    public final String getSelfCleaningDescription() {
+        return getSelfCleaning().getDescription();
+    }
+
+	public final void setSelfCleaningDescription(String description) {
+		if(unboundedGroups == null) {
+			unboundedGroups = new ArrayList<>(1);
+		}
+		if(CollectionUtils.isEmpty(unboundedGroups)) {
+			unboundedGroups.add(new DynamicRequirementGroup());
+		}
+		setFirstUnboundedGroupField("selfCleaningDescription", description);
+	}
+
+    public final boolean getSelfCleaningAnswer() {
+    	SelfCleaning selfCleaning = getSelfCleaning();
+        return selfCleaning != null && Boolean.TRUE.equals(selfCleaning.getAnswer());
+    }
+
+	public final void setSelfCleaningAnswer(boolean answer) {
+		if(unboundedGroups == null) {
+			unboundedGroups = new ArrayList<>(1);
+		}
+		if(CollectionUtils.isEmpty(unboundedGroups)) {
+			unboundedGroups.add(new DynamicRequirementGroup());
+		}
+		unboundedGroups.get(0).setAnswer(answer);
+	}
+	@Override
+	public void setSelfCleaning(SelfCleaning value) {
+		if(value != null) {
+			if(unboundedGroups == null) {
+				unboundedGroups = new ArrayList<>(1);
+			}
+			if(CollectionUtils.isEmpty(unboundedGroups)) {
+				unboundedGroups.add(new DynamicRequirementGroup());
+			}
+			unboundedGroups.get(0).setAnswer(value.getAnswer());
+			unboundedGroups.get(0).put("selfCleaningDescription", value.getDescription());
+		}
+	}
+	@Override
+	public SelfCleaning getSelfCleaning() {
+		if(CollectionUtils.isEmpty(unboundedGroups)) {
+			return null;
+		}
+		SelfCleaning sc = new SelfCleaning();
+		sc.setAnswer(unboundedGroups.get(0).getAnswer());
+		if(unboundedGroups.get(0).get("selfCleaningDescription") != null) {
+			sc.setDescription((String)unboundedGroups.get(0).get("selfCleaningDescription"));
+		}
+		return sc;
+	}
 }
